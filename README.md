f249a317327525f19ef4064f071599076d782579
tang汤 <5367952+tyng@user.noreply.gitee.com>
2019-10-19 15:12:56 +0800

    修改了README.md 和 main.js

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # 这是本人的第一个关于Vue的项目
 ## [五种开源协议的比较(BSD，Apache，GPL，LGPL，MIT)](http://www.ha97.com/833.html)
+### 用（传统方式）命令行把修改过后的代码上传到GitHub？？？
+1. git add .
+2. git commit -m "提交信息"
+3. git push
--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import './lib/mui/css/mui.min.css'
 import { Header } from 'mint-ui'
 Vue.component( Header.name, Header )
 
-// 导入 App 根组件
+// 导入 App 根组件  
 import app from './App.vue'
 
 var vm = new Vue({
